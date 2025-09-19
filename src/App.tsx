38a0fec80d391aaf253c0e6f35131902635a9460
eodudrepublic <eodudrepublic@gmail.com>
2025-09-19 17:26:37 +0900

    test(matchCard): 현재 작업 중인 `기본 카드`를 제외한 나머지 카드들을 주석처리

--- src/App.tsx
+++ src/App.tsx
@@ -136,17 +136,17 @@ function App() {
         />
 
         {/* 사람 수 표시가 있는 카드 */}
-        <MatchCard
+        {/* <MatchCard
           title="장소: 부산대 넥서스 E동"
           time="8/16 18:00 ~ 22:00"
           showPeopleCount={true}
           peopleCount="8/10"
           deadline="08/10 23:59"
           onCardClick={() => console.log('사람 수 카드 클릭됨')}
-        />
+        /> */}
 
         {/* 두 개 버튼이 있는 카드 */}
-        <MatchCard
+        {/* <MatchCard
           title="장소: 부산대 넥서스 E동"
           time="8/13 18:00 ~ 22:00"
           buttons={[
@@ -162,26 +162,26 @@ function App() {
             },
           ]}
           onCardClick={() => console.log('버튼 카드 클릭됨')}
-        />
+        /> */}
 
         {/* 결과 버튼이 있는 카드 */}
-        <MatchCard
+        {/* <MatchCard
           title="장소: 부산대 넥서스 E동"
           time="8/3 18:00 ~ 22:00"
           resultButton={true}
           onResultClick={() => console.log('결과 버튼 클릭')}
           onCardClick={() => console.log('결과 카드 클릭됨')}
-        />
+        /> */}
 
         {/* 이미지 없는 카드 */}
-        <MatchCard
+        {/* <MatchCard
           title="장소: 부산대 넥서스 E동"
           time="8/16 18:00 ~ 22:00"
           showPeopleCount={true}
           peopleCount="8/10"
           deadline="08/10 23:59"
           onCardClick={() => console.log('이미지 없는 카드 클릭됨')}
-        />
+        /> */}
       </S.Container>
     </>
   );
