4c0b08e1f19c1384e5c0385ed74a4ea20c22c573
eodudrepublic <eodudrepublic@gmail.com>
2025-10-31 17:56:48 +0900

    feat(api): Axios + React Query 연동 - `main.tsx`에 QueryClientProvider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,36 @@
 import { ThemeProvider } from '@emotion/react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+import { queryClient } from '@/api/core/queryClient';
 import App from '@/App.tsx';
 import GlobalStyles from '@/styles/GlobalStyles.tsx';
 import { theme } from '@/theme';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 );
